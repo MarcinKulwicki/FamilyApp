3832f8e48f971d30e83065d01b3a36ee30d32a82
Marcin Kulwicki <MarcinKulwicki@protonmail.com>
2018-09-13 16:17:43 +0200

    refactor

--- src/main/java/pl/marcinkulwicki/controller/restFamilyController.java
+++ src/main/java/pl/marcinkulwicki/controller/restFamilyController.java
@@ -2,7 +2,6 @@ package pl.marcinkulwicki.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import pl.marcinkulwicki.DTO.ChildDTO;
 import pl.marcinkulwicki.DTO.FamilyDTO;
@@ -33,8 +32,8 @@ public class restFamilyController {
     @PostMapping("/familyAdd")
     public void addFamily(@RequestBody FamilyDTO familyDTO) {
 
-        if(familyDTO.getFatherDTO() == null) throw new Error("Father Details are incorrect");
-        if(familyDTO.getChildrenDTO() == null) throw new Error("Child Details are incorrect");
+        if (familyDTO.getFatherDTO() == null) throw new Error("Father Details are incorrect");
+        if (familyDTO.getChildrenDTO() == null) throw new Error("Child Details are incorrect");
         FatherDTO fatherDTO = familyDTO.getFatherDTO();
         List<ChildDTO> childList = familyDTO.getChildrenDTO();
 
@@ -47,27 +46,25 @@ public class restFamilyController {
         } else {
             throw new Error("Father Details are incorrect");
         }
-
     }
 
     @PostMapping("/search")
-    public List<ChildDTO> searchFamily(@RequestBody ChildDTO childDTO){
+    public List<ChildDTO> searchFamily(@RequestBody ChildDTO childDTO) {
 
         List<ChildDTO> childs = familyService.searchChild(childDTO);
         childs.sort((o1, o2) -> o1.getSecondName().compareToIgnoreCase(o2.getSecondName()));
         sess.setAttribute("childsFind", childs);
-
         return childs;
     }
 
     @GetMapping("/search")
-    public FamilyDTO showFamily(@Param("id") Long id, Model model){
+    public FamilyDTO showFamily(@Param("id") Long id) {
         FamilyDTO familyDTO = new FamilyDTO();
-        if(id != null) familyService.readFamily(id);
+        if (id != null) familyService.readFamily(id);
 
         familyDTO.setFatherDTO((FatherDTO) sess.getAttribute("father"));
 
-        List<ChildDTO> childs = (List<ChildDTO>)sess.getAttribute("children");
+        List<ChildDTO> childs = (List<ChildDTO>) sess.getAttribute("children");
         childs.sort(Comparator.comparing(ChildDTO::getSecondName));
         familyDTO.setChildrenDTO(childs);
 
